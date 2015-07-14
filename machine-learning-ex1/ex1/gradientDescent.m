89dc304b0dec295c55c2568f5ae5869723a97b80
Joshua Hayes <joshuahayes@Joshuas-MacBook-Pro.local>
2015-07-14 18:18:51 +0800

    completed gradient-descent, amended cost-function

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -7,27 +7,31 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
-for iter = 1:num_iters
+    for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
+        % ====================== YOUR CODE HERE ======================
+        % Instructions: Perform a single gradient step on the parameter vector
+        %               theta. 
+        %
+        % Hint: While debugging, it can be useful to print out the values
+        %       of the cost function (computeCost) and gradient here.
+        %
+        temp_theta = zeros(length(theta),1);
+        
+        for index = 1:length(theta)
+            temp_theta(index) = theta(index) - (alpha/m) * sum(((X*theta)-y).*X(:,index));
+        end
+        
+        theta = temp_theta;
+        % ============================================================
 
+        % Save the cost J in every iteration    
+        J_history(iter) = computeCost(X, y, theta);
 
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCost(X, y, theta);
-
-end
-
+    end
+    
+    % Visualise decreasing cost function
+%     figure();
+%     plot(1:num_iters, J_history, '-');
+    
 end
