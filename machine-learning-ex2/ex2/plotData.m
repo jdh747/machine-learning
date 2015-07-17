e1f4bb0415433d4542b3b2c65da806d767cbb94c
Joshua Hayes <joshuahayes@Joshuas-MacBook-Pro.local>
2015-07-17 18:10:37 +0800

    updated plotData & sigmoid functions

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -10,16 +10,16 @@ figure; hold on;
 % Instructions: Plot the positive and negative examples on a
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
-%
-
-
-
-
-
-
-
-
-
+%   
+    % FIND RETURNS ARRAY OF INDICIES MEETING ARGUMENT COND.
+    positive_set = find(y==1); negative_set = find(y==0);
+    
+    %PLOT POSITIVE SET
+    plot(X(positive_set,1), X(positive_set,2), 'k+');
+    %PLOT NEGATIVE SET
+    plot(X(negative_set,1), X(negative_set,2), 'ko', 'MarkerFaceColor', 'y');
+    legend('Admitted', 'Not Admitted');
+    axis([30 100 30 100]);
 % =========================================================================
 
 
